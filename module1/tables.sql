36fb431f3d7970fcf19e9b5dc64d1f01fda1fa36
Aymen <nemya9066@gmail.com>
2025-03-26 15:48:06 +0000

    adding chats,chat_users,messages tables and message_view,car_view views

--- module1/tables.sql
+++ module1/tables.sql
@@ -44,4 +44,33 @@ create table if not exists car_listing
     constraint fk_iduser foreign key (id_user) references users (id)
 );
 
-commit;
+create table if not exists chats
+(
+    id bigint primary key
+);
+create table if not exists chat_users(
+    chat_id bigint not null,
+    user_id int not null,
+    primary key (chat_id, user_id),
+    constraint fk_chatid foreign key (chat_id) references chats(id),
+    constraint fk_userid foreign key (user_id) references users(id)
+);
+create table if not exists messages(
+    id serial primary key ,
+    chat_id bigint not null,
+    user_id int not null,
+    number int not null,
+    content text not null,
+    constraint fk_chatid foreign key (chat_id) references chats(id),
+    constraint fk_userid foreign key (user_id) references users(id)
+);
+create or replace view message_view as
+    select chat_users.chat_id as chat_id, chat_users.user_id as user_id, messages.id as message_id, messages.number as number, messages.content as content
+    from chat_users, messages
+    where chat_users.chat_id = messages.chat_id and chat_users.user_id = messages.user_id;
+
+create or replace view car_view as
+    select car_listing.id_car as car_id, car_listing.id_user as user_id, car_brand.name as brand_name,
+    car_model.name as model_name, car_model.production_year as year, cars.km_dirven as km
+    from car_listing, cars, car_model, car_brand
+    where car_listing.id_car = cars.id and cars.id_model = car_model.id and car_model.id_brand = car_model.id;
